0622dfa09fa33e305b9106f6a939ff1064301395
PFilip <2008filip@wp.pl>
2023-08-18 20:30:41 +0200

    e

---
+++ frontend/src/components/strapi-api/reklamies.jsx
@@ -0,0 +1,32 @@
+import useFetch from "../../scripts/useFetch";
+
+const Reklamies = (props) => {
+  // const { loading, error, data } = useFetch("http://127.0.0.1:1337/api/reklamies")
+  const { loading, error, data } = useFetch(`http://127.0.0.1:1337/api/${props.collection}`);
+
+  if (loading) return console.log("ladowanie");
+  if (error) return console.log("cos poszlo kurwa nie tak");
+
+  const response = data.data;
+  console.log(response);
+
+  console.log("test");
+
+  return (
+    <section id={'ad'} className={'ad_section'}>
+      {/*<h1>reklamies</h1>*/}
+      <div className={"ad_div"} id="ad_div">
+        {response.map((li) => (
+          <>
+            <a key={li.id} href={li.attributes.link}>
+              {li.attributes.nazwa}
+            </a>
+            <br />
+          </>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Reklamies;
---
+++ frontend/src/pages/404.jsx
@@ -0,0 +1,77 @@
+const NotFound = () => {
+  return (
+      <div>
+  <h1 className={'center'} style={{ position: "absolute", left: "0", right: "0", top: "0", bottom: "0"}}>Nie znaleziono kurva strony.</h1>
+  <p className={'center'}>
+    it startsz łith łan thing
+    aj don't knoł łaj
+    it dosynt iwyn matter hoł hard ju traj
+    kip that in majnd aj disajnt dis rajm
+    tu explajn in dułe tajm al aj noł
+    tajm i waluejbyl thing
+    łacz it flaj baj as de pendulum słings
+    łacz it kaunt dał tu di end of the dej
+    De klok tiks lajf ałej
+    it's soł unril
+    Didn't luuk aut beloł
+    łacz de tajm goł rajt aut de łindoł
+    trajin tu hold on
+    didn't iwyn knoł
+    I łejsted it ol just to łacz ju go
+
+    aj kept ewrything insajd
+    iwyn dhoł aj trajd
+    it oll fell apart
+    łhat it ment tu mi łill evyntualli bi
+    a memori of tajm łen aj trajd soł hard
+
+    aj trajd soł hard and gat soł far
+    bat in di end it doesn't iwyn matter
+    Aj had to fall tu luz it all
+    bat in di end it doesn't iwyn matter
+
+    łan thing aj don't knoł łhaj
+    it doesn't iwyn matter hoł hard ju traj
+    kip that in majnd aj dizajnt dis rajm
+    tu remajnd maj self hał aj trajd soł hard
+    in szpajt of de łej ju łer moking mi
+    Akting lajk aj łos part of jur properti
+    Remembering oll de tajms ju fougt łith mi
+    ajm suprajst it got so far
+    Things aren't de łej thej łej befor
+    ju wudn't iwyn rekognajs mi enimor
+    not that ju kneł mi bak den
+    But it oll koms bak tu mi in di end
+
+    ju kept ewrything insajd
+    and iwyn though aj trajd it oll fell apart
+    łhat it ment tu mi łill ewyntualli bi
+    ej memori of a tajm when aj trajd soł hard
+
+    aj trajd soł hard and gat soł far
+    bat in di end it doesn't iwyn matter
+    Aj had to fall tu luz it all
+    bat in di end it doesn't iwyn matter
+
+    ajv put maj trust in ju
+    puszt as far as aj kan go
+    For oll this
+    deris onli łan thing ju szud noł
+    ajv put maj trust in ju
+    puszt as far as aj kan go
+    For oll this
+    deris onli łan thing ju szud noł
+
+    aj trajd soł hard and gat soł far
+    bat in di end it doesn't iwyn matter
+    Aj had to fall tu luz it all
+    bat in di end it doesn't iwyn matter
+
+    -------
+    By PFilip
+  </p>
+      </div>
+  );
+};
+
+export default NotFound;
